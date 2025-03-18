622c489894020697f6f1e3a3b6cda54bd0b14a3e
Igor Lemeshkin <igorlem@icloud.com>
2025-03-18 12:58:01 +0700

    add schemas for better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://irrlmm.github.com",
+  site: "https://irrlmm.github.io",
   integrations: [react(), mdx(), sitemap()],
   experimental: {
     responsiveImages: true,
---
+++ src/consts/schemas.ts
@@ -0,0 +1,71 @@
+import type { Person, WithContext } from "schema-dts";
+
+export const SCHEMA_PERSON_MINI: WithContext<Person> = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Igor Lemeshkin",
+  jobTitle: "Product Designer",
+  url: "https://irrlmm.github.io",
+  sameAs: [
+    "https://www.linkedin.com/in/irrlmm",
+    "https://www.behance.net/irrlmm",
+    "https://dribbble.com/irrlmm",
+  ],
+};
+
+export const SCHEMA_PERSON_FULL: WithContext<Person> = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Igor Lemeshkin",
+  jobTitle: "Product Designer",
+  url: "https://irrlmm.github.io",
+  sameAs: [
+    "https://www.linkedin.com/in/irrlmm",
+    "https://dribbble.com/irrlmm",
+    "https://www.behance.net/irrlmm",
+  ],
+  hasOccupation: {
+    "@type": "Occupation",
+    name: "Lead Product Designer",
+    skills: ["UX/UI Design", "Design Systems"],
+  },
+  worksFor: {
+    "@type": "Organization",
+    name: "iMusician",
+    url: "https://imusician.pro",
+  },
+  alumniOf: {
+    "@type": "CollegeOrUniversity",
+    name: "Higher School of Economics",
+    alternateName: "HSE",
+    url: "https://www.hse.ru/en",
+  },
+  hasCredential: {
+    "@type": "EducationalOccupationalCredential",
+    name: "Bachelor's Degree in Business Informatics",
+  },
+  knowsAbout: ["Design", "UX/UI Design", "Product Design", "Frontend"],
+  skills: [
+    "Figma",
+    "Sketch",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "Rapid Prototyping",
+    "Design",
+    "Visual Design",
+    "Icon Design",
+    "Illustration Design",
+    "UI Design",
+    "UX Design",
+    "Design Systems",
+    "Product Design",
+    "Interaction Design",
+    "Frontend Development",
+    "Collaboration",
+    "Leadership",
+    "Analytics",
+  ],
+  award: ["Winner of Microsoft Imagine Cup Hackathon"],
+};
