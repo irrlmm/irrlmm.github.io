958bbcc0c16558d521de3223b442246db79062d9
Igor Lemeshkin <igorlem@icloud.com>
2025-04-23 19:17:59 +0700

    added: - astro-favicons so now they should generate automagically

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/consts/meta";
 
+import favicons from "astro-favicons";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [react(), mdx(), sitemap()],
+  integrations: [react(), mdx(), sitemap(), favicons()],
   experimental: {
     responsiveImages: true,
   },
