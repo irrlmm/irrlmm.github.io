341c8be2522023063ff02e7a1cff9c7a1e46a9e7
Igor Lemeshkin <igorlem@icloud.com>
2026-01-14 17:16:57 +0300

    updated images

--- src/consts/projects/highlights.ts
+++ src/consts/projects/highlights.ts
@@ -4,7 +4,7 @@ import reedsySpider from "../../assets/work/reedsy/spider.png";
 import reedsyNoir from "../../assets/work/reedsy/noir.png";
 
 // instant mastering
-import ishigsEmpty from "../../assets/work/ishigs/empty.png";
+import ishigsStep2 from "../../assets/work/ishigs/step-2.png";
 import ishigsModal from "../../assets/work/ishigs/modal-large.png";
 
 // artist hub
@@ -36,7 +36,7 @@ const PROJECTS_HIGHLIGHTS: UI.Project[] = [
       },
       {
         src: reedsyNoir,
-        chip: "AI assistants need a personality and distinct voice. I designed a set of cats to elevate each writer's personal style.",
+        chip: "AI assistants need a personality and distinct voice. I created a set of cats to elevate each writer's personal style.",
       },
     ],
     // link: {
@@ -44,61 +44,31 @@ const PROJECTS_HIGHLIGHTS: UI.Project[] = [
     //   href: "/work",
     // },
   },
-  // {
-  //   id: "imusician",
-  //   title: "iMusician",
-  //   timeline: "2017-2025",
-  //   lines: [
-  //     "Music distribution is boring. No one had unlocked its true potential - until we tried.",
-  //     "I challenged the status quo, directing product design to turn a simple tool into a SaaS powerhouse for modern musicians.",
-  //   ],
-  //   imgs: [
-  //     {
-  //       src: imdHome,
-  //       chip: "Home screen dashboard",
-  //     },
-  //     { src: imdSidewindows, chip: "Music library management" },
-  //     {
-  //       src: imdCheckout,
-  //       chip: "Reworked checkout experience increased successful payments by ~7% and reduced churn through clearer pricing, trusted payment methods, and simpler payment management.",
-  //     },
-  //   ],
-  //   link: {
-  //     label: "View",
-  //     href: "/work",
-  //   },
-  // },
   {
     id: "imusician-im",
     title: "iMusician: Instant Music Mastering, powered by AI",
     timeline: "2025",
     lines: [
       "Audio mastering used to be slow, complex, and expensive.",
-      "GenAI changed that.",
-      "I led the design of a new in-house mastering product, shipped in two months and optimized for fast user activation and business impact.",
+      "A shift in users' behavior and GenAI tech changed that.",
+      "I led the design of a new in-house mastering product, shipped in two months and optimized for fast user activation and virality.",
     ],
-    imgs: [{ src: ishigsEmpty }, { src: ishigsModal }],
+    imgs: [
+      {
+        src: ishigsStep2,
+        chip: "Mastered previews are generated almost instantly, making comparison frictionless.",
+      },
+      {
+        src: ishigsModal,
+        chip: "One in four users adopted the new Reference Mastering feature, proving its value.",
+      },
+    ],
     // link: {
     //   label: "View",
     //   href: "/work",
     //   // external: true,
     // },
   },
-  // {
-  //   id: "imusician-ds",
-  //   title: "iMD Design System",
-  //   timeline: "2017-2025",
-  //   lines: [
-  //     "Built for scalability and versatility from day one.",
-  //     "Started as a code-only Storybook collection in our frontend monorepo.",
-  //     "Later evolved in Figma empowering anyone be a designer and rapidly craft prototypes at any resolution.",
-  //   ],
-  //   imgs: [{ src: imdDSFields }, { src: ishigsComponents }],
-  //   link: {
-  //     label: "View",
-  //     href: "/work",
-  //   },
-  // },
   {
     id: "imusician-ah",
     title: "iMusician: Artist Hub",
@@ -109,7 +79,6 @@ const PROJECTS_HIGHLIGHTS: UI.Project[] = [
       "Powered by market research and user insights, built for visibility and growth.",
     ],
     imgs: [
-      // ap0,
       {
         src: apPages,
         chip: "Artist Page • A WYSIWYG editor and website publishing platform for musicians to showcase their work, bio, tour dates, and merch.",
@@ -127,7 +96,7 @@ const PROJECTS_HIGHLIGHTS: UI.Project[] = [
     lines: [
       "Self-driven explorations into new UX paradigms and industries.",
       "Recently researched automotive HMI systems, learning Blender and real-time rendering to prototype infotainment UIs.",
-      "I'd love to work on an automotive project someday.",
+      "I’d love to work on a project where analog controls and software design flexibility are fused together.",
     ],
     imgs: [
       { src: cofefi, chip: "Specialty coffee recipes app • iOS" },
@@ -136,8 +105,6 @@ const PROJECTS_HIGHLIGHTS: UI.Project[] = [
         src: mbuxHome,
         chip: "Mercedes-Benz Cockpit Dashboard concept • Linux-based vehicle OS ",
       },
-      // { src: mbuxMenu, chip: "MBUX Menu" },
-      // { src: mbuxMenuScheme, chip: "Mercedes-Benz Cockpit Menu Scheme • Vehicle UX exploration • Midjourney & Figma" },
     ],
   },
 ];
