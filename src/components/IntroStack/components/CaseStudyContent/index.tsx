ca673d3ebef6fcc9f0f550045f795a731cb6a2c2
Igor Lemeshkin <igorlem@icloud.com>
2026-01-19 21:54:22 +0300

    various fixes

--- src/components/IntroStack/components/CaseStudyContent/index.tsx
+++ src/components/IntroStack/components/CaseStudyContent/index.tsx
@@ -10,7 +10,7 @@ const CaseStudyContent: React.FC<Props> = ({ card: { title, link } }) => {
   return (
     <div className={styles.caseStudyCard}>
       <div className="col gap-m">
-        <h2>NEW</h2>
+        <h2>Read now</h2>
         <p>{title}</p>
       </div>
 
